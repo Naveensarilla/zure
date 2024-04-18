a7c004419edc25b8c7dcecf1981e80c0fd318112
Naveensarilla <naveen.s@egradtutor.in>
2024-04-18 17:39:53 +0530

    updating landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from "./egate_logo.png";
 import "./App.css";
+import { Egate } from "./Egate landing page/Egate";
 
 function App() {
   return (
     <div className="App">
-      <h1> Mr.Kevin </h1>
+      {/* <h1> Mr.Kevin </h1>
       <p>Succeeded</p>
       <h2>hello azure</h2>
-      <img src={logo} alt="logo " />
+      <img src={logo} alt="logo " /> */}
+<Egate />
+
     </div>
   );
 }
