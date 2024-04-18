193f936e96aac4f273279f74595e9f6b62075ead
Naveensarilla <naveen.s@egradtutor.in>
2024-04-18 18:11:28 +0530

    Revert "updating landing page"
    This reverts commit a7c004419edc25b8c7dcecf1981e80c0fd318112.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
+import logo from "./egate_logo.png";
 import "./App.css";
-import { Egate } from "./Egate landing page/Egate";
 
 function App() {
   return (
     <div className="App">
-      {/* <h1> Mr.Kevin </h1>
+      <h1> Mr.Kevin </h1>
       <p>Succeeded</p>
       <h2>hello azure</h2>
-      <img src={logo} alt="logo " /> */}
-<Egate />
-
+      <img src={logo} alt="logo " />
     </div>
   );
 }
