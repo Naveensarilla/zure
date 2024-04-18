2e99fc5eb86954672b59cbdb5a99e1c02e142781
Naveensarilla <naveen.s@egradtutor.in>
2024-04-18 14:50:26 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <p>Succeeded</p>
       <h2>hello azure</h2>
 
-   <img  src={logo} alt='logo ' />
+   <img  src='http://localhost:3003/static/media/egate%20logo%201.8b1eb31347d60945925f.png' alt='logo ' />
     </div>
   );
 }
