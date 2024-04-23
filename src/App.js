d3e9f90e9f328e1cc9ded0637282cafbd13b49b7
Naveensarilla <naveen.s@egradtutor.in>
2024-04-23 13:21:23 +0530

    removing name

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ function App() {
     <div className="App">
      
       <p>Succeeded</p>
+      <h1>
+        Mr.Kevin
+      </h1>
       <h2>hello azure</h2>
       <img width={200} height={200} src={logo} alt="logo " />
       <span> </span>
-  
+
       <p>Succeeded</p>
+
       <h2>hello azure</h2>
       <img width={200} height={200} src={logo} alt="logo " />
       <span> </span>
+
     </div>
   );
 }
