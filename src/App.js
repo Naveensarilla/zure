575fea822d37ed18501a676f526b59fc562c8b37
Naveensarilla <naveen.s@egradtutor.in>
2024-04-23 10:34:55 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Mr.Kevin </h1>
+     
       <p>Succeeded</p>
       <h2>hello azure</h2>
       <img width={200} height={200} src={logo} alt="logo " />
       <span> </span>
-      <h1> Mr.Kevin </h1>
+  
       <p>Succeeded</p>
       <h2>hello azure</h2>
       <img width={200} height={200} src={logo} alt="logo " />
